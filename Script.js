6592198f265b2fbd38bc82f5b358a7c0197c9ac5
AbdullahLebzo <abd.lebzo@gmail.com>
2020-12-22 19:10:53 +0200

    third

--- Script.js
+++ Script.js
@@ -5,21 +5,59 @@ window.addEventListener('DOMContentLoaded', (event) => {
       getTopRated(data);
     });
 
+  $(() => {
+    $('.slider').slick({
+      dots: false,
+      infinite: false,
+      speed: 300,
+      slidesToShow: 5,
+      slidesToScroll: 5,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true,
+          },
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+          },
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+          },
+        },
+      ],
+      prevArrow: '<span class="material-icons arrows priv-arrow">arrow_back</span>',
+      nextArrow: '<span class="material-icons arrows next-arrow">arrow_forward</span>',
+    });
+  });
+
   const topTitle = [];
   const topAvg = [];
   const topPoster = [];
   const topRelease = [];
 
   function getTopRated(data) {
-    for (let i = 0; i < 5; i += 1) {
-      topTitle.push(data.results[i].original_title);
+    for (let i = 0; i < data.results.length; i += 1) {
+      topTitle.push(data.results[i].title);
       topAvg.push(data.results[i].vote_average);
       topPoster.push(data.results[i].poster_path);
       topRelease.push(data.results[i].release_date);
     }
     // Append movies
-    for (let i = 0; i < 5; i += 1) {
+    for (let i = 0; i < data.results.length; i += 1) {
       const div = document.createElement('DIV');
+      div.classList.add('items');
       //   append rating
       const rating = document.createElement('SPAN');
       const movieRating = document.createTextNode(`${topAvg[i]}`);
@@ -34,18 +72,24 @@ window.addEventListener('DOMContentLoaded', (event) => {
       const movieName = document.createTextNode(`${topTitle[i]}`);
       title.appendChild(movieName);
       div.appendChild(title);
+      //   append movie icon
+      const movieIcon = document.createElement('SPAN');
+      movieIcon.innerHtml = '<span class="material-icons">play_circle_outline</span>';
+      movieIcon.classList.add('movie-icon');
+      div.appendChild(movieIcon);
       //   append release date
       const relDate = document.createElement('SPAN');
       const date = document.createTextNode(`${topRelease[i]}`);
       relDate.appendChild(date);
       div.appendChild(relDate);
-      document.querySelector('.top-movies').appendChild(div);
-      console.log(poster);
+      div.firstChild.classList.add('ratings');
+      div.lastChild.classList.add('rel-date');
+      document.querySelector(`.slider-item${i}`).appendChild(div);
     }
   }
 
-  console.log(topTitle);
-  console.log(topAvg);
-  console.log(topPoster);
-  console.log(topRelease);
+  // console.log(topTitle);
+  // console.log(topAvg);
+  // console.log(topPoster);
+  // console.log(topRelease);
 });
